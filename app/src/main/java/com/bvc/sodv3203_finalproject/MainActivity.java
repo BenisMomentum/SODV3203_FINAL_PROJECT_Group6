13cc20e9118639c0bb93c63d2173e1949bf9e25e
Leiziane Trevisan Dardin <leiziane.trevisan@yahoo.com.br>
2025-03-14 22:19:41 -0600

    created settings, create an account, forgotten password, set password, search workout pages layout

--- app/src/main/java/com/bvc/sodv3203_finalproject/MainActivity.java
+++ app/src/main/java/com/bvc/sodv3203_finalproject/MainActivity.java
@@ -9,15 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bvc.sodv3203_finalproject.user.Manager;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activty_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
